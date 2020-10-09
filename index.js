1e78c7d32dad75832ff141e147d0b13a464c74e0
jordan <jordan@onlineimage.com>
2020-10-08 20:33:32 -0600

    adding e2e to npm scripts

--- index.js
+++ index.js
@@ -6,14 +6,3 @@ app.listen(config.port, () => {
 });
 
 module.exports = app;
-
-// 1. Setup webpack, packages, server, config, etc.
-// 2. Create Endpoint Tests
-// 3. Create E2E tests (nightwatch)
-// 4. Write Endpoints
-// - Validation
-// - Model - Controller
-// - Views /login, /account
-// 5. Write Front end
-// -
-// 6. Hash, Salt, Secret, passwords.
